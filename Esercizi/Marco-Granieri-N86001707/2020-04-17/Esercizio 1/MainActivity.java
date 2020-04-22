b424317be9142bc82e56f96a0ddd971ccf526d37
Ram4096 <60160187+Ram4096@users.noreply.github.com>
2020-04-22 12:44:51 +0200

    Add files via upload

---
+++ Esercizi/Marco-Granieri-N86001707/2020-04-17/Esercizio 1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.esercizio1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String LOG ="APPANDROID";
+    public boolean checkVisibility(int currentTextView, int nextTextView) {
+        TextView text = findViewById(currentTextView);
+        if (text.isShown()){
+            Log.i(LOG, String.valueOf(currentTextView));
+            text.setVisibility(TextView.INVISIBLE);
+            findViewById(nextTextView).setVisibility(TextView.VISIBLE);
+            return true;
+        }
+        return false;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Esercizio1 - MARCO GRANIERI");
+        Button botton = findViewById(R.id.button);
+        botton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(LOG, "Bottone premuto");
+                int[] textViews = new int[] {R.id.textView1, R.id.textView2, R.id.textView3, R.id.textView4, R.id.textView5, R.id.textView1};
+                for (int i = 0; i < 5; i++) {
+                    if (checkVisibility(textViews[i], textViews[i+1]))
+                        break;
+                }
+            }
+        });
+    }
+}
