8cfd2741f691356f8809b711edab2129768013d6
Carmine Testa <carmine.testa97@libero.it>
2020-04-23 17:37:41 +0200

    Commit Esercizio 2

--- Esercizi/Carmine-Testa-N86002676/17-04-2020/Esercizio 2/app/src/main/java/com/example/esercizio2/MainActivity.java
+++ Esercizi/Carmine-Testa-N86002676/17-04-2020/Esercizio 2/app/src/main/java/com/example/esercizio2/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.esercizio2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView T1 = (TextView) findViewById(R.id.textView1);
+        final TextView T2 = (TextView) findViewById(R.id.textView2);
+
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.leftMargin =  80;
+                params.topMargin =  500;
+                T1.setLayoutParams(params);
+
+            }
+        });
     }
+    public int randomNumber(){
+        return (int) Math.random();
+    }
+
 }
