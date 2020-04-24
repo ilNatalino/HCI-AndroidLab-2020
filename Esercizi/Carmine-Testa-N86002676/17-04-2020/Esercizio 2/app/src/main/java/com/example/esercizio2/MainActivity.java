67b0764a0ef92f2fbedfae4ed4808f0eeb71ae7d
Carmine Testa <carmine.testa97@libero.it>
2020-04-24 11:16:45 +0200

    Aggiunto cambiamento continuo della posizione del testo

--- Esercizi/Carmine-Testa-N86002676/17-04-2020/Esercizio 2/app/src/main/java/com/example/esercizio2/MainActivity.java
+++ Esercizi/Carmine-Testa-N86002676/17-04-2020/Esercizio 2/app/src/main/java/com/example/esercizio2/MainActivity.java
@@ -17,23 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView T1 = (TextView) findViewById(R.id.textView1);
-        final TextView T2 = (TextView) findViewById(R.id.textView2);
 
         Button b = (Button)findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //System.out.println((int)Math.random());
                 RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                params.leftMargin =  80;
-                params.topMargin =  500;
+                params.leftMargin =  randomNumber(500);
+                params.topMargin =  randomNumber(800);
                 T1.setLayoutParams(params);
-
             }
         });
     }
-    public int randomNumber(){
-        return (int) Math.random();
+    public int randomNumber(int limite){
+        return (int) (Math.random()*limite);
     }
 
 }
